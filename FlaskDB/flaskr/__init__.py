70136e49cfd623917920f56d466a08cbadf56b4d
rmsolari <rmsolari@uc.cl>
2017-06-29 11:52:33 -0400

    jajajaj

--- FlaskDB/flaskr/__init__.py
+++ FlaskDB/flaskr/__init__.py
@@ -73,7 +73,7 @@ def numero_y_entero(numero, k):
 def aparece_palabra(palabra):
     result = list()
     contador=0
-    for tupla in mongodb.escuchas.find({"$text": {"$search": '\"{}\"'.format(palabra)}}, {"_id": 0, "$id": 0}):
+    for tupla in mongodb.escuchas.find({"$text": {"$search": palabra}}, {"_id": 0, "$id": 0}):
         contador+=1
         result.append(tupla)
         if contador == 10:
